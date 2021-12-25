0cc0b72f40ca4c198331756f5602834cfb8d30dd
emoacht <emotom@pobox.com>
2021-12-26 02:19:52 +0900

    Initial commit

---
+++ Source/RoundProgressBar/BorderInlineConverter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace RoundProgressBar
+{
+	internal class BorderInlineConverter : IMultiValueConverter
+	{
+		public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+		{
+			if (values is { Length: 4 } &&
+				values[0] is (double width and >= double.Epsilon) &&
+				values[1] is (double height and >= double.Epsilon) &&
+				values[2] is CornerRadius radius &&
+				values[3] is Thickness thickness)
+			{
+				var rect = new Rect(0, 0, (width - thickness.Left - thickness.Right), (height - thickness.Top - thickness.Bottom));
+				var radiusX = radius.TopLeft - thickness.Left / 2D;
+				var radiusY = radius.TopLeft - thickness.Top / 2D;
+
+				var clip = new RectangleGeometry(rect, radiusX, radiusY);
+				clip.Freeze();
+
+				return clip;
+			}
+
+			return DependencyProperty.UnsetValue;
+		}
+
+		public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+		{
+			throw new NotSupportedException();
+		}
+	}
+}
---
+++ Source/StripedProgressBar/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace StripedProgressBar
+{
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private async void Start_Click(object sender, RoutedEventArgs e)
+		{
+			try
+			{
+				this.StartButton.IsEnabled = false;
+				this.Slider.IsEnabled = false;
+
+				this.Bar.Value = 0;
+
+				while (this.Bar.Value < 100D)
+				{
+					await Task.Delay(TimeSpan.FromMilliseconds(50));
+
+					this.Bar.Value += (this.Bar.Value < 90) ? 1 : 0.25;
+				}
+			}
+			finally
+			{
+				this.StartButton.IsEnabled = true;
+				this.Slider.IsEnabled = true;
+			}
+		}
+	}
+}
