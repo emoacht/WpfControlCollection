60258b62321244c25f83b88e92074e96d531b2d3
emoacht <emotom@pobox.com>
2022-07-23 21:07:36 +0900

    Add Switchable ComboBox

---
+++ Source/SwitchableComboBox/MainWindowViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Toolkit.Mvvm.ComponentModel;
+
+namespace SwitchableComboBox
+{
+	public enum Rank { Normal, Second, First };
+
+	public class MainWindowViewModel : ObservableObject
+	{
+		public IEnumerable<Rank> Ranks { get; } = Enum.GetValues<Rank>();
+
+		public ObservableCollection<MemberViewModel> Members { get; } = new();
+
+		public MainWindowViewModel()
+		{
+			foreach (var name in new[] { "Fuki", "Takina", "Sakura", "Erica", "Hibana" })
+				Members.Add(new MemberViewModel(name));
+		}
+	}
+
+	public class MemberViewModel : ObservableObject
+	{
+		public string Name { get; }
+
+		public Rank Rank
+		{
+			get => _rank;
+			set => SetProperty(ref _rank, value);
+		}
+		private Rank _rank;
+
+		public MemberViewModel(string name) => Name = name ?? throw new ArgumentNullException(nameof(name));
+	}
+}
---
+++ Source/SwitchableComboBox/MouseFocusableTextBlock.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace SwitchableComboBox
+{
+	public class MouseFocusableTextBlock : TextBlock
+	{
+		public MouseFocusableTextBlock() : base()
+		{
+			Focusable = true;
+
+			MouseDown += (_, _) => { Keyboard.Focus(this); };
+		}
+	}
+}
