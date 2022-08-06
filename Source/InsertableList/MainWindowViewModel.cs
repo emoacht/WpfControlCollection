2c8849ca2165f9a656466d83aaa0a83c77ddad7f
emoacht <emotom@pobox.com>
2022-08-06 09:09:56 +0900

    Replace Microsoft.Toolkit with CommunityToolkit

--- Source/InsertableList/MainWindowViewModel.cs
+++ Source/InsertableList/MainWindowViewModel.cs
@@ -5,8 +5,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
-using Microsoft.Toolkit.Mvvm.ComponentModel;
-using Microsoft.Toolkit.Mvvm.Input;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 
 namespace InsertableList
 {
--- Source/SwitchableComboBox/MainWindowViewModel.cs
+++ Source/SwitchableComboBox/MainWindowViewModel.cs
@@ -4,7 +4,7 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Microsoft.Toolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.ComponentModel;
 
 namespace SwitchableComboBox
 {
