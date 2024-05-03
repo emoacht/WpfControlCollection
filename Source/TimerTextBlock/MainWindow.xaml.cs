f64ceaf0cc8df8e0e9ad86a7ca6c815ae6eaa109
emoacht <emotom@pobox.com>
2024-05-03 13:04:02 +0900

    Add Timer TextBlock

---
+++ Source/TimerTextBlock/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Windows;
+
+namespace TimerTextBlockDemo;
+
+public partial class MainWindow : Window
+{
+	public ObservableCollection<DateTime> Times { get; } = [];
+
+	public MainWindow()
+	{
+		InitializeComponent();
+
+		var dateTime = DateTime.Now;
+		Times.Add(dateTime);
+		Times.Add(dateTime + TimeSpan.FromSeconds(1));
+		Times.Add(dateTime + TimeSpan.FromSeconds(2));
+	}
+}
---
+++ Source/TimerTextBlock/TimerTextBlock.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Threading;
+
+namespace TimerTextBlockDemo;
+
+public class TimerTextBlock : TextBlock
+{
+	private static DispatcherTimer _timer;
+	private string? _format;
+
+	static TimerTextBlock()
+	{
+		_timer = new() { Interval = TimeSpan.FromSeconds(Interval) };
+		_timer.Start();
+	}
+
+	public static double Interval { get; set; } = 1D;
+
+	public DateTime Time
+	{
+		get { return (DateTime)GetValue(TimeProperty); }
+		set { SetValue(TimeProperty, value); }
+	}
+	public static readonly DependencyProperty TimeProperty =
+		DependencyProperty.Register(
+			"Time",
+			typeof(DateTime),
+			typeof(TimerTextBlock),
+			new PropertyMetadata(default(DateTime), OnTimeChanged));
+
+	private static void OnTimeChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+	{
+		((TimerTextBlock)d).IsRunning = ((DateTime)e.NewValue != default);
+	}
+
+	private bool _isRunning;
+	public bool IsRunning
+	{
+		get => _isRunning;
+		set
+		{
+			if (_isRunning != value)
+			{
+				_isRunning = value;
+				if (_isRunning)
+				{
+					var expression = BindingOperations.GetBindingExpression(this, TimeProperty);
+					_format = expression.ParentBinding.StringFormat;
+
+					_timer.Tick += OnTick;
+				}
+				else
+				{
+					_timer.Tick -= OnTick;
+				}
+			}
+		}
+	}
+
+	private void OnTick(object? sender, EventArgs e)
+	{
+		this.Text = (DateTime.Now - Time).ToString(_format);
+	}
+}
